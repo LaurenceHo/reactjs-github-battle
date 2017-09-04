fbc227f313847725dd21243dcc1236d54a5c9252
Laurence Ho <bluegray.he@aol.com>
2017-09-04 13:19:59 +1200

    Populate the popular page

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -4,7 +4,7 @@ import { Popular } from './popular';
 export class App extends React.Component {
 	render () {
 		return (
-      <div class='container'>
+      <div className='container'>
           <Popular/>
       </div>
 		);
